f7f43d58bf424a49cfebdb8a39270c943a2cf650
wacupid22 <9118768@qq.com>
2020-01-17 21:13:40 +0800

    update

--- src/ngrok/auth/auth.go
+++ src/ngrok/auth/auth.go
@@ -22,7 +22,7 @@ func SetSecretPath(path string) {
 }
 
 func (this *MyAuth) Auth(token string) bool {
-	log.Info("wacupid token: ========> " + token)
+	c.conn.Info("wacupid token: ========> ", token)
 	fields := strings.Split(token, ":")
 	if len(fields) != 2 {
 		log.Info("wrong format of token")
--- src/ngrok/server/control.go
+++ src/ngrok/server/control.go
@@ -85,7 +85,8 @@ func NewControl(ctlConn conn.Conn, authMsg *msg.Auth) {
 	}
 
 	// auth the username and password
-	log.Info("wacupid token: ========> " + authMsg.User)
+	c.conn.Info("wacupid authMsg: ========> ", authMsg)
+	c.conn.Info("wacupid user: ========> ", authMsg.User)
 	tokenAuth := auth.New()
 	if tokenAuth.Auth(authMsg.User) == false {
 		failAuth(errors.New("Invalid token"))
